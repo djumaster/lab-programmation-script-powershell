afc0ded1df88b64b51ccd96f1277487c6b469490
djumaster <djumaster@gmail.com>
2020-04-02 16:20:31 -0400

    djumaster

--- b000000000-fonction.ps1
+++ b000000000-fonction.ps1
@@ -14,8 +14,16 @@
 # Definition de la fonction
 function Stagiaire {
 
+ param (
+        [String]$personneNom,
+        [Int]$personneAge
+    )
+
+# message de bienvenue 
+    "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge
+
 }
 
 # Appel de la fonction
-Stagiaire
+Stagiaire --VILLE-- --AGE--
 
